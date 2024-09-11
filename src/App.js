baae98a829fd74f0740d6f494f5295c9278a1296
Shivam Kumar Singh <krantike@gmail.com>
2024-09-11 23:24:14 +0530

    Commit All

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import {MapContainer, Marker, Tooltip, Popup, TileLayer, Circle} from 'react-leaflet'
+import L from "leaflet"
+import 'leaflet/dist/leaflet.css';
+import LibraryData from "../src/libraryData.json.json"
+import { useEffect, useState } from 'react';
+import Footer from './Footer';
+
+
 
 function App() {
+
+  const [searchQuery, setSearchQuery] = useState('')
+  
+  const fetchedLibrary = () => {
+   if(searchQuery) {
+    return LibraryData.filter(data => data.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+     )
+   }
+  return LibraryData;
+ }
+
+ useEffect(()=>{
+  fetchedLibrary()
+ }, [searchQuery])
+
+
+const getOpacityByPopulation = (population) => {
+  const minPopulation = 1000;   // Example minimum population
+  const maxPopulation = 12000000; // Example maximum population
+  return (population - minPopulation) / (maxPopulation - minPopulation) * 0.9 + 0.1;
+};
+
+
+
+const customIcon = L.icon({
+  iconUrl: 'https://i0.wp.com/bhumkalsamachar.com/wp-content/uploads/2019/03/Inquilab-Zindabad-1.png?ssl=1',
+  iconSize: [38, 38], // Size of the icon
+  iconAnchor: [19, 38], // Point where the icon should anchor (coordinates for centering the icon)
+  popupAnchor: [0, -38], // Position of the popup relative to the icon
+});
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+<input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search for a particular library'>
+</input>
+
+   
+    <div>
+      <MapContainer center={[23.5, 88.67]} zoom={4.8} style={{width: "100vw" , zIndex: 10}}>
+      <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+       {fetchedLibrary().map((lib)=> (
+          <Circle center={[lib.lat ,lib.long]} radius={lib.radius || 10000} opacity={getOpacityByPopulation(lib.population)} fillColor="blue" color="transparent" bindlabel={"test"}>
+           <Tooltip>Population Size : {lib.population}</Tooltip> 
+        
+        <Marker position={[lib.lat ,lib.long]} icon={customIcon}>
+
+        <Popup><h4>{lib.name}</h4>
+          {lib.address}
+          <p>Contact Email : {lib.contact_email}</p>
+        </Popup>
+      </Marker>
+      </Circle>
+       ))}
+        
+      </MapContainer>
+    </div>
+    <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+
+const Footer = () => {
+  return (
+    <div className="text-white">
+        <h5 style={{display: "flex" , alignContent: "center", justifyContent: "center"}}>All rights reserve to <a style={{marginLeft: 5}} href='https://www.fln.org.in'>Free Libraries Network FLN</a></h5>
+
+    </div>
+  )
+}
+
+export default Footer
